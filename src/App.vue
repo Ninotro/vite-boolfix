43523a761e45530fa5d543141507e65f89a62da3
Ninotro <ninotroia96@gmail.com>
2023-06-07 14:56:27 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import { store } from "./store.js";
+import AppHeader from './components/AppHeader.vue';
+import ListCard from "./components/ListCard.Vue"
+
+
+export default {
+  components: {
+    AppHeader,
+    ListCard
+    
+    
+  },
+  data () {
+    return {
+      store
+    }
+  }
+
+}
+  
+
+
+</script>
+
+<template>
+ <header>
+   <AppHeader/>
+ </header>
+ <main>
+  <ListCard/>
+ </main>
+</template>
+
+<style lang="scss" >
+@use "../src/styles/general.scss";
+main {
+  background-color: grey;
+  height: 1000px;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+ 
+ name : "AppHeader",
+
+
+}
+
+
+</script>
+
+<template>
+  <div id="container-header">
+    <div>BOOLFIX</div>
+    <input type="text" placeholder="Inserisci il nome del film">
+  </div>
+ 
+</template>
+
+<style lang="scss" scoped>
+@use "../src/styles/general.scss";
+
+#container-header {
+  height: 250px;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  div {
+    color: red;
+    font-size: 50px;
+    margin-left: 50px;
+  }
+
+  input {
+    color: black;
+   
+    height: 50px;
+    margin-right: 50px;
+    font-size: 30px;
+    padding-left: 20px;
+   
+  }
+}
+</style>
